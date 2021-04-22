bf4f84e206c98f943d8fc85e213b5a8315f267df
W1nterFr3ak <winterfreak@protonmail.com>
2021-04-22 19:28:07 +0300

    multiple wordcount

---
+++ multwc.py
@@ -0,0 +1,59 @@
+def coroutine(fn):
+    def wrapper(*args, **kwargs):
+        c = fn(*args, **kwargs)
+        next(c)
+        return c
+    return wrapper
+
+
+def cat (f, casesensitive, child):
+    if casesensitive:
+        process = lambda k: k
+    else:
+        process = lambda k: k.lower()
+
+    for l in f:
+        child.send(process(l))
+
+@coroutine
+def grep(pattern, casesensitive, child):
+    if not casesensitive:
+        pattern = pattern.lower()
+
+    while True:
+        text = (yield)
+        child.send(text.count(pattern))
+
+@coroutine
+def multc(children):
+    while True:
+        data = (yield)
+        for child in children:
+            child.send(data)
+@coroutine
+def count(pattern):
+    n = 0
+    try:
+        while True:
+            n += (yield)
+
+    except GeneratorExit:
+        print(pattern, n)
+
+
+
+
+if __name__ == "__main__":
+
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-i', action='store_true', dest='casesensitive')
+    parser.add_argument("pattern", type=str, nargs="+",)
+    parser.add_argument("infile", type=argparse.FileType('r'))
+
+    args = parser.parse_args()
+
+
+    cat(args.infile, args.casesensitive, multc([grep(pattern, args.casesensitive, count(pattern))for pattern in args.pattern]))
+
