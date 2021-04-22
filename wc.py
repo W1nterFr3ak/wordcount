f0e6b0ddacf05eb12ebb60f9bb9f5a5a043a2a5d
W1nterFr3ak <winterfreak@protonmail.com>
2021-04-22 17:59:42 +0300

    added grep and cat plus coroutine wrapper

--- wc.py
+++ wc.py
@@ -1,1 +1,29 @@
 import argparse
+
+def coroutine(fn):
+    def wrapper(*args, **kwargs):
+        c = fn(*args, **kwargs)
+        next(c)
+        return c
+    return wrapper
+
+
+def cat (f, casesensitive, child):
+    if casesensitive:
+        process = lambda k: k
+    else:
+        process = lambda k: k.lower()
+
+    for l in f:
+        child.send(process(l))
+
+
+def grep(pattern, casesensitive, child):
+    if not casesensitive:
+        pattern = pattern.lower()
+
+    while True:
+        text = (yield)
+        child.send(text.count(pattern))
+
+
